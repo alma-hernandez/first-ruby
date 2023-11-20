578c70bed90e6227cf3595a6b055b86485f67ff1
Alma Duffey <1984ahern@gmail.com>
2023-11-20 12:54:37 -0600

    created new book-harry potter

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
     skip_before_action :verify_authenticity_token
     def create
-        render json: { name: 'Nash' }
-        
+        new_book = Book.create(name: params[:name])
+        render json: { book: new_book }
     end
 end
