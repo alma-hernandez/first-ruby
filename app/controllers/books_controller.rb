726768049459e66c35b271191b740f1ea1e9ca0e
Alma Duffey <1984ahern@gmail.com>
2023-11-22 06:22:05 -0600

    completed walkthrough

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
     skip_before_action :verify_authenticity_token
     def create
-        new_book = Book.create(name: params[:name])
-        render json: { book: new_book }
+        @book = Book.create(name: params[:name])
+        render 'books/create.jbuilder'
     end
 end
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
-    describe 'POST /books' do
-        it 'create a new book object' do
-          post :create, params: { name: 'Harry Potter' }
-          expect(Book.count).to eq(1)
-        end
-      end
+  describe 'POST /books' do
+    it 'create a new book object' do
+      ...
+    end
+
+    it 'responds with a book object' do
+      post :create, params: {name: 'Harry Potter'}
+
+      expected_response = {
+        book: {
+          name: 'Harry Potter'
+        }
+      }
+
+      expect(response.body).to eq(expected_response.to_json)
+    end
+  end
 end
